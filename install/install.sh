e302e6c06d7f9784abca4715ce2246ee34e79459
Daniel Chong <danielgq.chong@gmail.com>
2016-04-07 12:45:36 +1200

    make sure karabiner private.xml is kept in my dotfile

--- install/install.sh
+++ install/install.sh
@@ -5,3 +5,10 @@ git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$H
 # Install Phoenix
 mix local.hex
 mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
+
+# Set the default key repeats
+defaults write -g InitialKeyRepeat -int 12
+defaults write -g KeyRepeat -int 2
+
+# Use Karabiner to use VI keys in preview
+ln -svf ~/.dotfiles/install/private.xml ~/Library/Application\ Support/Karabiner/private.xml
