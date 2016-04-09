150f347ccedff599b43a3aae7da3cad5c7b178e5
Daniel Chong <danielgq.chong@gmail.com>
2016-04-09 14:52:48 +1200

    clone elixir and erlang source code

--- install/install.sh
+++ install/install.sh
@@ -12,3 +12,7 @@ defaults write -g KeyRepeat -int 2
 
 # Use Karabiner to use VI keys in preview
 ln -svf ~/.dotfiles/install/private.xml ~/Library/Application\ Support/Karabiner/private.xml
+
+# Clone Elixir source so we have access to documentation in emacs
+git clone https://github.com/elixir-lang/elixir.git /usr/local/sources/elixir
+git clone https://github.com/erlang/otp.git /usr/local/sources/erlang
