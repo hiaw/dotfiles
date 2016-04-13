726424fc09319cae477dc55e4f330096ae6a8694
Daniel Chong <danielgq.chong@gmail.com>
2016-04-14 08:56:09 +1200

    added clean trash script, install tex etc

---
+++ install/clean_trash.sh
@@ -0,0 +1,12 @@
+rm -rf ~/.Trash/*.pkg
+rm -rf ~/.Trash/*.app
+rm -rf ~/.Trash/*.apk
+rm -rf ~/.Trash/*.ipa
+rm -rf ~/.Trash/*.docset
+
+rm -rf ~/.Trash/*.tar
+rm -rf ~/.Trash/*.zip
+rm -rf ~/.Trash/*.bz2
+rm -rf ~/.Trash/*.gz
+
+rm -rf ~/.Trash/*.torrent
