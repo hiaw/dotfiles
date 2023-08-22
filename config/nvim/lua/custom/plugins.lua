af40460fe379853b0067e6a9acaab917231bd924
Daniel Chong <danielgq.chong@gmail.com>
2023-08-22 16:29:45 +1200

    comment out rust related stuff

--- config/nvim/lua/custom/plugins.lua
+++ config/nvim/lua/custom/plugins.lua
@@ -62,20 +62,20 @@ local plugins = {
     end, -- Override to setup mason-lspconfig
   },
 	"dense-analysis/ale",
-	"simrat39/rust-tools.nvim",
+	-- "simrat39/rust-tools.nvim",
       -- Completion framework:
-    'hrsh7th/nvim-cmp' ,
+    -- 'hrsh7th/nvim-cmp' ,
 
     -- LSP completion source:
-    'hrsh7th/cmp-nvim-lsp',
+    -- 'hrsh7th/cmp-nvim-lsp',
 
     -- Useful completion sources:
-    'hrsh7th/cmp-nvim-lua',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
-    'hrsh7th/cmp-vsnip'                   ,          
-    'hrsh7th/cmp-path'                     ,         
-    'hrsh7th/cmp-buffer'                    ,        
-    'hrsh7th/vim-vsnip',
+    -- 'hrsh7th/cmp-nvim-lua',
+    -- 'hrsh7th/cmp-nvim-lsp-signature-help',
+    -- 'hrsh7th/cmp-vsnip'                   ,          
+    -- 'hrsh7th/cmp-path'                     ,         
+    -- 'hrsh7th/cmp-buffer'                    ,        
+    -- 'hrsh7th/vim-vsnip',
 
   -- To make a plugin not be loaded
   -- {
