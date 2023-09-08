3d8e15865c2c130c50447b309bf2166f1538cc1c
Daniel Chong <danielgq.chong@gmail.com>
2023-09-09 11:24:13 +1200

    mapping options n to open up tree.

--- config/nvim/lua/custom/mappings.lua
+++ config/nvim/lua/custom/mappings.lua
@@ -2,24 +2,26 @@
 local M = {}
 
 M.custom = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
 
-    [","] = { ":HopWord<CR>", "Hop word" },
+		[","] = { ":HopWord<CR>", "Hop word" },
 
-    ["<C-`>"] = {
-      function()
-        require("nvterm.terminal").toggle "horizontal"
-      end,
-      "Toggle floating term",
-    },
+		["<C-`>"] = {
+			function()
+				require("nvterm.terminal").toggle("horizontal")
+			end,
+			"Toggle floating term",
+		},
 
-    ["<leader>v"] = { ":vsplit<CR>", "Split Vertical" },
+		["<leader>v"] = { ":vsplit<CR>", "Split Vertical" },
 
-    ["<A-f>"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
+		["<A-f>"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
 
-    ["<A-p>"] = { "<cmd> Telescope find_files <CR>", "Find files" },
-  },
+		["<A-p>"] = { "<cmd> Telescope find_files <CR>", "Find files" },
+
+		["<A-n>"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
+	},
 }
 
 -- more keybinds!
