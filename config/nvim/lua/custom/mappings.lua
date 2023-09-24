48f5c6b1ba59ee160b0df39d6037ab3ed032334b
Daniel Chong <danielgq.chong@gmail.com>
2023-09-24 22:53:15 +1300

    adding telescope grep_string

--- config/nvim/lua/custom/mappings.lua
+++ config/nvim/lua/custom/mappings.lua
@@ -7,16 +7,13 @@ M.custom = {
 
 		[","] = { ":HopWord<CR>", "Hop word" },
 
-		["<C-`>"] = {
-			function()
-				require("nvterm.terminal").toggle("horizontal")
-			end,
-			"Toggle floating term",
-		},
-
 		["<leader>v"] = { ":vsplit<CR>", "Split Vertical" },
 
 		["<A-f>"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
+
+		["<leader>fs"] = { "<cmd> Telescope grep_string <CR>", "Grep String" },
+
+		["<A-S-f>"] = { "<cmd> Telescope grep_string <CR>", "Grep String" },
 
 		["<A-p>"] = { "<cmd> Telescope find_files <CR>", "Find files" },
 
