3746e5467f267131d6aef416059d10ef59749eb3
Daniel Chong <danielgq.chong@gmail.com>
2023-09-25 14:57:24 +1300

    enable vim folding using treesitter

--- config/nvim/lua/custom/init.lua
+++ config/nvim/lua/custom/init.lua
@@ -2,6 +2,11 @@
 
 vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
 
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+vim.opt.foldenable = false
+
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
