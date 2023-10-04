8d367c9d596db670752b8550b4719861f709e74a
Daniel Chong <danielgq.chong@gmail.com>
2023-10-04 16:53:15 +1300

    adding text-case

---
+++ config/nvim/lua/plugins/text-case.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "johmsalas/text-case.nvim",
+    keys = { "<leader>ga", { "<leader>ga", desc = "text-case", mode = "v" } },
+    config = function()
+      require("textcase").setup({})
+    end,
+  },
+}
