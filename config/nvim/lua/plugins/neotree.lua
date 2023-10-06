a9816c29e19303fc282e8773529c867d61c256cd
Daniel Chong <danielgq.chong@gmail.com>
2023-10-06 22:09:48 +1300

    using tab to open file or folder in neo tree

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -2,6 +2,11 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      window = {
+        mappings = {
+          ["<tab>"] = "open",
+        },
+      },
       filesystem = {
         filtered_items = {
           hide_dotfiles = false,
