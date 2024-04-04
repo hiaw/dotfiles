5e1251f35b0286dc52ec37512e5ff6d8822cbab7
Daniel Chong <danielgq.chong@gmail.com>
2024-04-04 23:06:19 +1300

    adding debugger

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
 
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.coding.codeium" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.json" },
