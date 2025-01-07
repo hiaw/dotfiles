e81d42dfb899d8b8210b962ed46834b9bec235f1
Daniel Chong <danielgq.chong@gmail.com>
2025-01-08 08:53:45 +1300

    zen mode is already install for lazyvim

--- config/nvim/lua/plugins/editor-all.lua
+++ config/nvim/lua/plugins/editor-all.lua
@@ -34,14 +34,14 @@ return {
   --   end,
   -- },
   --
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = { "hrsh7th/cmp-emoji" },
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     table.insert(opts.sources, { name = "emoji" })
+  --   end,
+  -- },
   {
     "neovim/nvim-lspconfig",
     opts = {
