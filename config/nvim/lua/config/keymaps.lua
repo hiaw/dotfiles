d882ed6d7ba35aa2351c63f93c49b23a5b724f24
Daniel Chong <danielgq.chong@gmail.com>
2025-02-18 15:33:07 +1300

    mapped comment to the correct key binding

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,11 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", ";", ":", { desc = "enter command mode", nowait = true })
+
+local map = LazyVim.safe_keymap_set
+map("n", "<C-/>", function()
+  return MiniComment.operator() .. "_"
+end, { desc = "Toggle Comment", expr = true })
+map("x", "<C-/>", function()
+  return MiniComment.operator()
+end, { desc = "Toggle Comment", expr = true })
