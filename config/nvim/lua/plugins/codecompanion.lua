7d5fa24340ed67bd62becfb8d42f8d42ea164246
Daniel Chong <danielgq.chong@gmail.com>
2025-05-01 16:48:08 +1200

    ensure mcp is running

--- config/nvim/lua/plugins/codecompanion.lua
+++ config/nvim/lua/plugins/codecompanion.lua
@@ -133,6 +133,16 @@ return {
         --   })
         -- end,
       },
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            make_vars = true,
+            make_slash_commands = true,
+            show_result_in_chat = true,
+          },
+        },
+      },
       strategies = {
         chat = {
           adapter = "copilot",
