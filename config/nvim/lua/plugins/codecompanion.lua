9f301e2499df817d67818bba6b27c0486f21551d
Daniel Chong <danielgq.chong@gmail.com>
2025-05-27 09:42:25 +1200

    remove copilot

--- config/nvim/lua/plugins/codecompanion.lua
+++ config/nvim/lua/plugins/codecompanion.lua
@@ -117,12 +117,12 @@ return {
           return require("codecompanion.adapters").extend("gemini", {
             schema = {
               model = {
-                default = "gemini-2.0-flash",
+                default = "gemini-2.5-flash-preview-05-20",
                 description = "Gemini model to use",
                 items = {
+                  "gemini-2.5-flash-preview-05-20",
                   "gemini-2.0-flash",
                   "gemini-1.5-pro",
-                  "gemini-2.5-pro-exp-03-25",
                 },
               },
             },
