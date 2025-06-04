4235e615f104697f4e13762b1004f3270914bd57
Daniel Chong <danielgq.chong@gmail.com>
2025-06-05 10:29:37 +1200

    adding wezterm

---
+++ runcom/wezterm.lua
@@ -0,0 +1,9 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
