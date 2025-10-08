06a6e158c26accbcb98daeaa0ec756f9caecfb76
Daniel Chong <danielgq.chong@gmail.com>
2025-10-08 17:46:24 +1300

    disable wezterm new tab keyboard shortcut

--- runcom/wezterm.lua
+++ runcom/wezterm.lua
@@ -7,4 +7,12 @@ local config = wezterm.config_builder()
 config.hide_tab_bar_if_only_one_tab = true
 config.font_size = 14
 
+config.keys = {
+	{
+		key = "t",
+		mods = "CMD",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
+
 return config
