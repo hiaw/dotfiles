14001737a624952b4db16b19d37ddff5b39db82e
Daniel Chong <danielgq.chong@gmail.com>
2026-05-10 23:03:04 +1200

    fixing wezterm weirdness

--- runcom/wezterm.lua
+++ runcom/wezterm.lua
@@ -2,7 +2,9 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.term = "wezterm"
+-- Keep TERM on a widely-installed entry. `wezterm` requires matching terminfo;
+-- when missing, zsh/readline-style redraw can corrupt interactive input.
+config.term = "xterm-256color"
 config.font = wezterm.font("Hack Nerd Font Mono")
 config.font_size = 14
 config.color_scheme = "tokyonight"
